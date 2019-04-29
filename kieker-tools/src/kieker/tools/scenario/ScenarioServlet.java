03e6effb53e8c59ecafa66b61591292565a2e9ca
IcedSoul <1443700905@qq.com>
2019-04-29 10:34:06 +0800

    delete build

---
+++ kieker-tools/src/kieker/tools/scenario/ScenarioServlet.java
@@ -0,0 +1,17 @@
+package kieker.tools.scenario;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author IcedSoul
+ * @date 19-4-29 上午10:25
+ */
+public class ScenarioServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp){
+
+    }
+
+}
